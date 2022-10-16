9314bcd8d939a3b2ae8bcb7273cb12ff89e027da
Mach1nka <uspenskiyart@gmail.com>
2022-10-17 01:31:06 +0300

    fix: add file extenstions for upload

--- src/components/upload-file/constant.ts
+++ src/components/upload-file/constant.ts
@@ -1,5 +1,7 @@
 const acceptableFileExtensions = [
   'application/msword',
+  'application/vnd.ms-excel',
+  'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
   'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
   'application/pdf',
 ];
