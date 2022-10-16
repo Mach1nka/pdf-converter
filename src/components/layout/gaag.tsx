7154c2c0163cf2915ce14b61c906f4e826817769
Mach1nka <uspenskiyart@gmail.com>
2022-10-17 00:56:26 +0300

    fix: fix file name

--- src/components/layout/gaag.tsx
+++ src/components/layout/gaag.tsx
@@ -1,7 +1,7 @@
 import { useContext } from 'react';
 import { LinearProgress } from '@mui/material';
 
-import { LoaderContext } from '../../contexts/angudiagn';
+import { LoaderContext } from '../../contexts/Loader';
 
 const Loader: React.FC = () => {
   const { loading } = useContext(LoaderContext);
